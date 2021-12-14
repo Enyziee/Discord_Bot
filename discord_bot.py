170d2e2263b09eca23b7b43b13352b73608ba466
Colomyers <lucascolomera@gmail.com>
2021-12-14 15:27:05 -0300

    Update discord_bot.py
    codigo comentado

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,17 @@
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+#libs utilizadas
 
-client = commands.Bot(command_prefix=".", )
+
+client = commands.Bot(command_prefix=".", ) 
+#prefixo utilizado para chamar o bot(.)
+
+
 load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+#token do bot
+
 
 def init_cogs():
     for cog in os.listdir("cogs/"):
@@ -16,6 +22,8 @@ def deact_cogs():
     for cog in os.listdir("cogs/"):
         if cog.endswith(".py"):
             client.unload_extension(f"cogs.{cog[:-3]}")
+#funcoes para iniciar e desligar os cogs
+
 
 @client.event
 async def on_ready():
@@ -23,29 +31,22 @@ async def on_ready():
     init_cogs()
     print("O Bot está pronto!")
     
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
-
-
 @client.command()
 async def load(ctx):
     init_cogs()
     await ctx.send("Cog loaded")
-
 
 @client.command()
 async def unload(ctx):
     deact_cogs()
     await ctx.send("Cog unloaded")
 
-
 @client.command()
 async def restart(ctx):
     deact_cogs()
     init_cogs()
     await ctx.send("Cog restarted")
+#funcoes para avisar quando o bot iniciar, as cogs estiverem ligadas, desligadas, ou quando forem reiniciadas respectivamente
 
 
 client.run(BOT_TOKEN)
